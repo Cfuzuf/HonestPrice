3b9b163867041958a980c9f7353e893b3d6717d5
Cfuzuf <cfuzuf@gmail.com>
2023-02-17 23:15:33 +0300

    Тестовые наброски

--- main.py
+++ main.py
@@ -1,44 +1,48 @@
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+import cv2
 import easyocr
+import os
 
 
-Builder.load_string('''
-<CameraClick>:
-    orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: True
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: text_input.text = root.capture()
-    TextInput:
-        id: text_input
-        text: ""
-''')
-
-
-class CameraClick(BoxLayout):
-    def capture(self):
-        camera = self.ids['camera']
-        # timestr = time.strftime("%Y%m%d_%H%M%S")
-        # camera.export_to_png("IMG_{}.png".format(timestr))
-        # print("Captured")
-        frame = camera.export_as_image()
-        frame.save("tmp.jpg")
-        reader = easyocr.Reader(["ru"])
-        text = " ".join(reader.readtext("tmp.jpg", detail=0, paragraph=True))
-        return text
-
-
-class TestCamera(App):
+class MainApp(App):
 
     def build(self):
-        return CameraClick()
+        self.camera = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.camera_update, 1/30)
+
+        bl_1 = BoxLayout(orientation="vertical")
+        self.img_1 = Image()
+        self.ti_1 = TextInput(size_hint=[1, 0.2])
+        btn_1 = Button(size_hint=[1, 0.1], on_press=self.btn_1_press)
+
+        bl_1.add_widget(self.img_1)
+        bl_1.add_widget(self.ti_1)
+        bl_1.add_widget(btn_1)
+        return bl_1
+
+    def camera_update(self, *args):
+        _, self.frame = self.camera.read()
+        buffer = cv2.flip(self.frame, 0).tostring()
+        self.texture = Texture.create(
+            size=(self.frame.shape[1], self.frame.shape[0]),
+            colorfmt="bgr"
+        )
+        self.texture.blit_buffer(buffer, colorfmt="bgr", bufferfmt="ubyte")
+        self.img_1.texture = self.texture
+
+    def btn_1_press(self, *args):
+        cv2.imwrite("/tmp.jpg", self.frame)
+        img_to_txt_reader = easyocr.Reader(lang_list=["ru"])
+        text = " ".join(img_to_txt_reader.readtext("/tmp.jpg", detail=0, paragraph=True))
+        self.ti_1.text = text
+        os.remove("/tmp.jpg")
 
 
-TestCamera().run()
+if __name__ == "__main__":
+    MainApp().run()
